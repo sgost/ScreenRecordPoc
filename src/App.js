71658c5ffb23a084deba00773efb4437d486f7ac
Manoj Ponugoti <jameslucky917@gmail.com>
2023-02-23 09:19:26 +0530

    changes done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { useReactMediaRecorder } from "react-media-recorder";
+import React, { useEffect } from 'react';
+import axios from 'axios';
+
 
 function App() {
+
+  const { status, startRecording, stopRecording, mediaBlobUrl } = useReactMediaRecorder({ screen: true });
+
+  useEffect(() => {
+    uploadVideoFun();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [status === "stopped"]);
+
+  const uploadVideoFun = () => {
+    let formData = new FormData();
+    if (mediaBlobUrl) {
+      fetch(mediaBlobUrl)
+        .then((res) => res.blob())
+        .then((blob) => {
+          const file = new File([blob], mediaBlobUrl, { type: blob.type })
+          console.log("fileBlob", file)
+          console.log("formData", formData)
+          formData.append('file', file);
+          formData.append('title', 'test title');
+          formData.append('desc', 'test desc');
+          axios({
+            url: `https://screen.fidisys.com/api/v1/fileUpload`,
+            method: 'POST',
+            data: formData
+          }).then((_res) => alert("Video Uploaded Successfully")).error((err) => alert("Oops!, Something went wrong"))
+        })
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className='pc_recorder'>
+      <h1>PC Screen Recorder</h1>
+      <div className='control_btns'>
+        {(status === "idle" || status === "stopped") &&
+          <button onClick={startRecording} className="start_btns">Start</button>}
+        {status === "recording" &&
+          <button onClick={stopRecording} className="start_btns stop_btns">Stop</button>}
+      </div>
     </div>
   );
 }
